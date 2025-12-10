d4281bf801d4f32aabd03daa033737c170d55e1b
ThaisTimoteo <Thaiskarine99@gmail.com>
2025-12-09 22:02:03 -0300

    1º commit

---
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.WebApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/thais")
+    public String greetingThais() {
+        return "Hello, Thais!";
+    }
+}
